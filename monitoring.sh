e0891c77ce60c13a8cac05cbfed77c9980e1319f
root <root@jkhong42.com>
2021-05-26 22:07:18 +0800

    changes made for final changes

--- monitoring.sh
+++ monitoring.sh
@@ -1,2 +1,45 @@
-./prompt.sh > status.txt
-wall < status.txt
+#!/bin/bash
+
+#Architecture
+uname -a | awk '{printf "#Architecture: " $0 "\n"}'
+
+#CPU physical
+nproc | awk '{printf "#CPU physical: " $0 "\n"}'
+
+#vCPU
+cat /proc/cpuinfo | grep processor | wc -l | awk '{printf "#vCPU: %d\n", $0}'
+
+#Memory usage
+free -m | awk 'NR == 2 {printf "#Memory Usage : %d/%dMB (%.2f%%)\n", $3, $2, $3/$2*100}'
+
+#Disk usage
+df --block-size=1048576 --total | grep '^/dev/' \
+	| awk '{total_space+=$2}{used_space+=$3}END{printf "#Disk Usage: %.1f/%.1fGb (%.f%%)\n", used_space/1000, total_space/1000, used_space/total_space*100}'
+
+#CPU usage
+top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" \
+	| awk '{printf "#CPU load: %.1f%%\n", 100 - $1}'
+
+#Last boot
+who -b | awk '{printf "#Last boot: %s %s\n", $3, $4}'
+
+#LVM use
+lsblk --list -o TYPE | grep lvm | wc -l | awk '{printf "#LVM use: "
+	if ($1 == "0")
+		print "no"
+	else
+		print "yes"
+	}'
+
+#TCP connections
+ss -s | grep TCP | awk 'NR==1 {printf "#TCP Connections: %d ESTABLISHED\n", $4}'
+
+#User log
+who | wc -l | awk '{printf "#User log: %d\n", $1}'
+
+#Network
+ip route | awk 'NR==2 {printf "#Network: IP %s ", $9}'
+ip addr show enp0s3 | grep "link/ether " | awk '{printf "(%s)\n", $2}'
+
+#Sudo
+ls /var/log/sudo/00/00 | wc -l | awk '{printf "#Sudo: %s cmd\n", $1}'
